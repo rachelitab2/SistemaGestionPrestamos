a2f46507a87c8d14d6b77c5bfcca6393abccca21
rache <rache@RACH>
2025-11-27 22:31:12 -0400

    Creacion de clases: datos, negocio

--- SistemaGestionPrestamos.Negocio/Usuarios01.cs
+++ SistemaGestionPrestamos.Negocio/Usuarios01.cs
@@ -1,6 +1,6 @@
 ﻿namespace SistemaGestionPrestamos.Negocio
 {
-    public class Class1
+    public class Usuarios01
     {
 
     }
--- SistemaGestionPrestamos/ConexionDatos.cs
+++ SistemaGestionPrestamos/ConexionDatos.cs
@@ -1,6 +1,6 @@
 ﻿namespace SistemaGestionPrestamos
 {
-    public class Class1
+    public class ConexionDatos
     {
 
     }
